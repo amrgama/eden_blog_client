7f7f6b24ce444552e26467bc93b0ccf1024e74b2
mohamedfathycreation <m.fathy@creation.sa>
2025-04-24 18:33:47 +0200

    update axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,14 @@
 import axios from "axios"
+// const PASE_URL = "https://edenblogapi-production.up.railway.app";
 const PASE_URL = "http://localhost:3500";
 
 export default axios.create({
     baseURL: PASE_URL,
     headers: {
+        'Content-Type': "application/json",
         'Access-Control-Allow-Origin' : '*'
     },
+    // withCredentials: true
 })
 
 export const axiosPrivate = axios.create({
